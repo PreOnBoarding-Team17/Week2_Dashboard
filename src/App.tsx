dda1ef0ae7498f29366d08cff4f047e6963a0051
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-07 21:36:28 +0900

    chore: 상위 컴포넌트에 네비게이션바 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'Utils/Styles/_reset.scss';
 import 'App.scss';
+import NavigationBar from 'Components/NavigationBar';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>hello world!</h1>
+      <NavigationBar />
     </div>
   );
 }
