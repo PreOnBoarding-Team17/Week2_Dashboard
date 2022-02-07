31cf9589ab522332a5c6f0b404c2eaafc39f2f89
developjik <developjik@gmail.com>
2022-02-08 00:39:24 +0900

    refactor: 변수 이름 및 값 변경

--- src/Utils/Constants/index.ts
+++ src/Utils/Constants/index.ts
@@ -2,14 +2,6 @@ const METHOD_NAME = '가공방식';
 const MATERIAL_NAME = '재료';
 const METHOD = ['밀링', '선반'];
 const MATERIAL = ['알루미늄', '탄소강', '구리', '합금강', '강철'];
-const CARD_ELEMENTS_NUMBER = ['도면개수', '총 수량'];
-const CARD_ELEMENTS_EXTRA = ['가공방식', '재료'];
+const CARD_ELEMENTS = ['도면개수', '총 수량', '가공방식', '재료'];
 
-export {
-  METHOD_NAME,
-  MATERIAL_NAME,
-  METHOD,
-  MATERIAL,
-  CARD_ELEMENTS_NUMBER,
-  CARD_ELEMENTS_EXTRA,
-};
+export { METHOD_NAME, MATERIAL_NAME, METHOD, MATERIAL, CARD_ELEMENTS };
