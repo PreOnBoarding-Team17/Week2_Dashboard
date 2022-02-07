586ab7b5f1fbc16b9c03045060a2bf5ca61861bc
som-syom <dhyeon@student.42seoul.kr>
2022-02-08 00:35:30 +0900

    refactor: isToogleSelect 상태를 부모로 옮김

--- src/Components/Common/Filter/index.tsx
+++ src/Components/Common/Filter/index.tsx
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import 'Components/Common/Filter/scss/Filter.scss';
 import { IFilter } from 'Utils/Interface';
-import ArrowDownIcon from 'Assets/ArrowDown.png';
 
-const Filter: React.FC<IFilter> = ({ title, name, options }) => {
-  const [isToggle, setIsToggle] = useState<boolean>(false);
+const Filter: React.FC<IFilter> = ({
+  title,
+  name,
+  options,
+  isToggleSelect,
+  setIsToggleSelect,
+  buttonRef,
+  onClickSelect,
+}) => {
+  const [count, setCount] = useState<number>(0);
+  const [selected, setSelected] = useState<string[]>([]);
 
-  const onClickSelect = () => {
-    // e.preventDefault();
-    console.log(!isToggle);
-    setIsToggle(!isToggle);
+  const onChangeCheckbox = (option: string) => {
+    if (selected.find((item: string) => item === option) === undefined) {
+      setSelected([...selected, option]);
+      setCount(count + 1);
+    } else {
+      setSelected(selected.filter((item: string) => item !== option));
+      setCount(count - 1);
+    }
+    setIsToggleSelect('');
+    buttonRef.current?.classList.remove('focused');
   };
 
   return (
@@ -18,18 +32,32 @@ const Filter: React.FC<IFilter> = ({ title, name, options }) => {
         <button
           type="button"
           className="select-item__title"
-          onClick={onClickSelect}
+          onClick={(e) => onClickSelect(e, name)}
+          ref={buttonRef}
         >
           {title}
-          <img src={ArrowDownIcon} alt="arrow down" />
+          {count > 0 && (
+            <span className="select-item__title__count">({count})</span>
+          )}
+          <span className="arrow-down-icon"></span>
         </button>
-        {isToggle && (
+        {isToggleSelect && (
           <ul className="select-item__checkbox-wrap">
             {options.map((option: string, index: number) => {
+              const isChecked =
+                selected.find((item: string) => item === option) !== undefined
+                  ? true
+                  : false;
               return (
                 <li key={index} className="checkbox-item">
-                  <input type="checkbox" id={option} />
-
+                  <input
+                    type="checkbox"
+                    id={option}
+                    name={option}
+                    value={option}
+                    onChange={(e) => onChangeCheckbox(e.target.value)}
+                    checked={isChecked}
+                  />
                   <label htmlFor={option}>
                     <span></span>
                     {option}
--- src/Components/Dashboard/FilterMenu.tsx
+++ src/Components/Dashboard/FilterMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { METHOD, METHOD_NAME, MATERIAL, MATERIAL_NAME } from 'Utils/Constants';
 import Filter from 'Components/Common/Filter';
 import Toggle from 'Components/Common/Toggle';
@@ -6,15 +6,56 @@ import 'Components/Dashboard/scss/FilterMenu.scss';
 
 function FilterMenu() {
   const [toggle, setToggle] = useState<boolean>(false);
+  const [isToggleSelect, setIsToggleSelect] = useState<string>('');
+  const methodRef = useRef<HTMLButtonElement>(null);
+  const materialRef = useRef<HTMLButtonElement>(null);
+
   const handleToggle = () => {
     setToggle(!toggle);
   };
+
+  const onClickSelect = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    name: string
+  ) => {
+    e.preventDefault();
+
+    if (name === 'method') {
+      methodRef.current?.classList.toggle('focused');
+    } else if (name === 'material') {
+      materialRef.current?.classList.toggle('focused');
+    }
+    if (isToggleSelect === '') {
+      setIsToggleSelect(name);
+    } else {
+      setIsToggleSelect('');
+      methodRef.current?.classList.remove('focused');
+      materialRef.current?.classList.remove('focused');
+    }
+  };
+
   console.log(toggle);
   return (
     <div className="filter">
       <div className="filter__select-item">
-        <Filter title={METHOD_NAME} name="method" options={METHOD} />
-        <Filter title={MATERIAL_NAME} name="material" options={MATERIAL} />
+        <Filter
+          title={METHOD_NAME}
+          name="method"
+          options={METHOD}
+          isToggleSelect={isToggleSelect === 'method'}
+          setIsToggleSelect={setIsToggleSelect}
+          buttonRef={methodRef}
+          onClickSelect={onClickSelect}
+        />
+        <Filter
+          title={MATERIAL_NAME}
+          name="material"
+          options={MATERIAL}
+          isToggleSelect={isToggleSelect === 'material'}
+          setIsToggleSelect={setIsToggleSelect}
+          buttonRef={materialRef}
+          onClickSelect={onClickSelect}
+        />
       </div>
       <div className="filter__toggle-item">
         <Toggle handleToggle={handleToggle} />
