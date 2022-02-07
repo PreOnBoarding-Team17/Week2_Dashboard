ff5f00f39fa51b0b97482abb3a507b147fb3a4e8
developjik <developjik@gmail.com>
2022-02-07 22:35:37 +0900

    feat: API 새로운 함수 추가

--- src/API/index.tsx
+++ src/API/index.tsx
@@ -3,6 +3,10 @@ import { DataInterface } from 'Utils/Interface';
 
 const BASE_URL = 'http://localhost:4000/requests';
 
+export const callAPI = axios.create({
+  baseURL: `${BASE_URL}`,
+});
+
 export const getAPI = async () => {
   const response = await axios.get<DataInterface>(BASE_URL);
   return response.data;
