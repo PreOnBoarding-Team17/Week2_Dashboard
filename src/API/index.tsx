3c1abd485f1606c36afd212a2ccb53e50d0feaee
developjik <developjik@gmail.com>
2022-02-09 02:22:17 +0900

    chore: 배포 설정

--- src/API/index.tsx
+++ src/API/index.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 import { DataInterface } from 'Utils/Interface';
 
-const BASE_URL = 'http://localhost:4000/requests';
+const BASE_URL = '/requests';
 
 export const callAPI: AxiosInstance = axios.create({
   baseURL: `${BASE_URL}`,
