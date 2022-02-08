ff6990a3b13a11be0abf6e7f4c9ba2b99d538863
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-08 21:50:54 +0900

    feat: useApi Hook 생성

--- src/API/index.tsx
+++ src/API/index.tsx
@@ -1,7 +1,9 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
 
 const BASE_URL = 'http://localhost:4000/requests';
 
-export const callAPI: AxiosInstance = axios.create({
-  baseURL: `${BASE_URL}`,
-});
+export const getAPI = async () => {
+  const response = await axios.get(BASE_URL);
+  const data = await response.data;
+  return data;
+};
