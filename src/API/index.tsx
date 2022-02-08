f47aa49920e9d214a8072c02e864910af5d1d4a8
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-08 18:04:30 +0900

    refactor: 사용하지 않는 API 코드 제거

--- src/API/index.tsx
+++ src/API/index.tsx
@@ -1,13 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
-import { DataInterface } from 'Utils/Interface';
 
 const BASE_URL = 'http://localhost:4000/requests';
 
 export const callAPI: AxiosInstance = axios.create({
   baseURL: `${BASE_URL}`,
 });
-
-export const getAPI = async () => {
-  const response = await axios.get<DataInterface>(BASE_URL);
-  return response.data;
-};
