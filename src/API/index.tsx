581952f24969426c928c842352757551a47f52ab
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-09 03:11:09 +0900

    chore: 로컬 실행 명령 수정

--- src/API/index.tsx
+++ src/API/index.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 import { DataInterface } from 'Utils/Interface';
 
-const BASE_URL = '/requests';
+const BASE_URL = 'http://localhost:4000/requests';
 
 export const callAPI: AxiosInstance = axios.create({
   baseURL: `${BASE_URL}`,
